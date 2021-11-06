a60c27528eb332f83c6ecdf4d1c9bb6f040690ff
Nicole Huang <nhuang@lcs.on.ca>
2021-11-06 11:11:03 -0400

    Initial Commit

---
+++ Collectible Cards Assignment/Collectible_Cards_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Collectible_Cards_AssignmentApp.swift
+//  Collectible Cards Assignment
+//
+//  Created by Nicole on 2021/11/6.
+//
+
+import SwiftUI
+
+@main
+struct Collectible_Cards_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
